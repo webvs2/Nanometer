3f38232ac168524ce211c2cb91b4b2db46ce3cc2
yang <29636098325@qq.com>
2022-10-29 15:55:11 +0800

    mx

--- synchronization.js
+++ synchronization.js
@@ -8,15 +8,33 @@ const readline = require("readline");
 let DEMAND = false;
 let base = "/hp";
 let sync = {
-  jquery: "https://code.jquery.com/jquery-3.6.1.min.js",
-  font_1141750_xwopum7j9a:
-    "https://at.alicdn.com/t/font_1141750_xwopum7j9a.eot",
-  font_1141750_xwopum7j9awoff2:
-    "https://at.alicdn.com/t/font_1141750_xwopum7j9a.woff2",
+  font_2584761_1u7dwraxww1:
+    "https://at.alicdn.com/t/font_2584761_1u7dwraxww1.css",
+  font_2584761_uyk2t2qdhl9:
+    "https://at.alicdn.com/t/font_2584761_uyk2t2qdhl9.css",
+  font_2735453_5qukyiw3qso:
+    "https://at.alicdn.com/t/font_2735453_5qukyiw3qso.css",
+  font_2735451_byy7g2xtc3p:
+    "https://at.alicdn.com/t/c/font_2735451_byy7g2xtc3p.css",
+  font_2584761_vultm9qpa3:
+    "https://at.alicdn.com/t/c/font_2584761_vultm9qpa3.css",
+  t1624882059084: "https://at.alicdn.com/t/font_2584761_1u7dwraxww1.ttf",
+  woff: "https://at.alicdn.com/t/font_2584761_1u7dwraxww1.woff",
+  woff2: "https://at.alicdn.com/t/font_2584761_1u7dwraxww1.woff2",
+  uyk2t2qdhl9Eot: "https://at.alicdn.com/t/font_2584761_uyk2t2qdhl9.eot",
+  uyk2t2qdhl9Woff: "https://at.alicdn.com/t/font_2584761_uyk2t2qdhl9.woff",
+  uyk2t2qdhl9Ttf: "https://at.alicdn.com/t/font_2584761_uyk2t2qdhl9.ttf",
+  uyk2t2qdhl9Svg: "https://at.alicdn.com/t/font_2584761_uyk2t2qdhl9.svg",
+  vultm9qpa3Eot: "https://at.alicdn.com/t/c/font_2584761_vultm9qpa3.eot",
+  byy7g2xtc3pEot: "https://at.alicdn.com/t/c/font_2735451_byy7g2xtc3p.eot",
+  qukyiw3qsoTtf: "https://at.alicdn.com/t/font_2735453_5qukyiw3qso.ttf",
+  qukyiw3qsoWoff: "https://at.alicdn.com/t/font_2735453_5qukyiw3qso.woff",
+  qukyiw3qsoWoff2: "https://at.alicdn.com/t/font_2735453_5qukyiw3qso.woff2",
 };
+let syncInfo = {};
 let acquire = (customization) => {
   let syncObj = customization || sync;
-  Object.keys(syncObj).map((item) => {
+  Object.keys(syncObj).map((item, index) => {
     let localUrl = path.join(__dirname, `${base}/${path.basename(sync[item])}`);
     fs.readFile(localUrl, function (err, data) {
       let DEMANDsubject = DEMAND ? !err : true;
@@ -32,7 +50,17 @@ let acquire = (customization) => {
                   sync[item]
                 )}\x1B[0m      `
             );
-            pathReplace(item,`${base}/${path.basename(sync[item])}`)
+            // ${base}
+            syncInfo[item] = path.basename(sync[item]);
+            // console.log(Object.keys(syncObj).length,index)
+            if (Object.keys(syncObj).length - 1 === index) {
+              Object.keys(syncInfo).map((j, i) => {
+                pathReplace(syncInfo[j],`./${path.basename(
+                  sync[item]
+                )}`);
+              });
+            }
+
           });
       } else {
         console.log(
@@ -43,7 +71,7 @@ let acquire = (customization) => {
   });
 };
 if (process.argv[2]) {
-  if(!sync[process.argv[2]])return false
+  if (!sync[process.argv[2]]) return false;
   inquirer
     .prompt([
       {
@@ -54,7 +82,7 @@ if (process.argv[2]) {
       },
     ])
     .then((answer) => {
-      acquire({[process.argv[2]]:sync[process.argv[2]]});
+      acquire({ [process.argv[2]]: sync[process.argv[2]] });
     });
 } else {
   inquirer
@@ -76,41 +104,46 @@ if (process.argv[2]) {
   const args = process.args;
 }
 
-function pathReplace(keyword,con,presentBase = "/src/",) {
+function pathReplace(keyword, con, presentBase = "/hp/") {
   fs.readdir(path.join(__dirname, presentBase), function (err, files) {
     if (err) {
       return console.error(err);
     }
     files.map((file) => {
       fs.stat(path.join(__dirname, presentBase + file), function (err, stats) {
+        if (err) {
+          return console.error(err);
+        }
         if (stats.isFile()) {
-          if (/(js|sass|vue)$/.test(path.extname(file))) {
-            fs.readFile(
+          if (/(html|js|css)$/.test(path.extname(file))) {
+            let data = fs.readFileSync(
               path.join(__dirname, presentBase + file),
-              "utf8",
-              function (err, data) {
-                let text = data.replace( new RegExp(  `${base}.+${keyword}`,'g') , con);
-                console.log(new RegExp(  `${base}.+${keyword}`,'g'))
-                fs.writeFile(
-                  path.join(__dirname, presentBase + file),
-                  text,
-                  function (err) {
-                    if (err) {
-                      return console.error(err);
-                    }
-                    readline.clearLine(process.stdout);
-                    process.stdout.write("数据写入成功！", "utf-8");
-                    readline.cursorTo(process.stdout,0);
-                  }
-                );
-              }
+              "utf8"
             );
+                fs.writeFileSync(
+                    path.join(__dirname, presentBase + file),
+                    data.replace(
+                      new RegExp(`(http|https)?:?\/\/+at.+${keyword}+(.+(?='|"))?`, "g"),
+                      function (context) {
+                        console.log(context)
+                        return con;
+                      }
+                    ),
+                    function (err) {
+                      if (err) {
+                        return console.error(err);
+                      }
+                      readline.clearLine(process.stdout);
+                      process.stdout.write("数据写入成功！", "utf-8");
+                      readline.cursorTo(process.stdout, 0);
+                    }
+                  );
           }
         } else if (stats.isDirectory()) {
-          pathReplace(keyword,con,presentBase + file + "/");
+          pathReplace(keyword, con, presentBase + file + "/");
         }
       });
     });
   });
 }
-// pathReplace('杨鹏飞','1212')
+// pathReplace('https://at.alicdn.com/t/font_2735453_5qukyiw3qso.ttf','杨鹏飞')
